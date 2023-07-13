fbce33c3344da27485b7678ad5089eb08c8735ae
Abhishek Kumar <abhisheksmscollege@gmail.com>
2023-07-13 15:21:11 +0530

    MyPortfolio

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div class="navbar footer">
+      <h2>&copy; 2023 Abhishek Kumar.Feel Free to Copy</h2>
+      <div class="menu">
+        <a href="#">LinkedIn</a>
+        <a href="#">Facebook</a>
+        <a href="#">Instagram</a>
+        <a href="#">Twitter</a>
+        <a href="#">GitHub</a>
+      </div>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style.css";
+
+const Navbar = () => {
+  return (
+    <div class="navbar">
+      <h1>Abhishek Kumar</h1>
+      <div class="menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Skills</a>
+        <a href="#">Qualifications</a>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
